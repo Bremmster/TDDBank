a85ff2a6b8e83086c79ea46d49e594c3192730db
Kristian Karlson <83krikar@gafe.molndal.se>
2023-05-23 11:22:29 +0200

    made bank

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -1,6 +1,54 @@
 package bank;
 
+import java.util.Scanner;
+
 public class Bank {
 
+    public Bank() {
+        Account usrAccount = new Account(0, "SWE-AXX-69420");
+        System.out.println("Account created! " + usrAccount.getACCOUNT_NUMBER() + "balance: " + usrAccount.getBalance());
+        mainMenu(usrAccount);
+    }
 
+    public void mainMenu(Account usrAccount) {
+
+        while (true) {
+            printOptions();
+            int usrChoice = getUsrInt();
+
+            switch (usrChoice) {
+                case 1 -> {
+                    System.out.println("Amount to deposit");
+                    usrAccount.deposit(getUsrInt());
+                }
+                case 2 -> {
+                    System.out.println("Amount to withdraw");
+                    usrAccount.withdraw(getUsrInt());
+                }
+                case 3 -> {
+                    System.out.println("Acc: " + usrAccount.getACCOUNT_NUMBER() + " Current Balance " + usrAccount.getBalance());
+                }
+                case 4 -> System.exit(0);
+            }
+        }
+    }
+
+    public void printOptions() {
+        System.out.println("1: Deposit cash\n2: Withdraw cash\n3: Check balance\n4: exit");
+    }
+
+    public int getUsrInt() {
+        Scanner scanner = new Scanner(System.in);
+        int usrChoice;
+
+        while (true) {
+            try {
+                usrChoice = scanner.nextInt();
+                return usrChoice;
+            } catch (Exception e) {
+                System.out.println("Not an Integer" + e.getMessage());
+            }
+        }
+    }
 }
+
